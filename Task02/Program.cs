a140e1811da3c5c0cfc2db6d232a5b7044a61cd6
Ksenia <kashilova@edu.hse.ru>
2020-05-05 18:12:48 +0300

    task 02 4

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -53,7 +53,8 @@ namespace Task02
                 double[]filteredCollection1 = Array.ConvertAll(filteredCollection, x => (double)x);
 
                 // использовать статическую форму вызова метода подсчета среднего
-                double averageUsingStaticForm = filteredCollection1.Aggregate((x, y) => (x*x+y*y)/2.0);
+                double 
+                double averageUsingStaticForm = filteredCollection1.Aggregate((x, y) => (x*x+y*y)/ filteredCollection1.Length);
                 Console.WriteLine($"{averageUsingStaticForm:f3}".Replace('.', ','));
                 // использовать объектную форму вызова метода подсчета среднего
                 double averageUsingInstanceForm = filteredCollection.ToList().ConvertAll(x => x*x).Average();
